b2d95a2fd808f2f325ac564d36d89bdd7c43d7f1
Rajwat Singh <singra01@gettysburg.edu>
2026-07-13 18:18:27 +0545

    Revert "changed the colorscheme"
    This reverts commit a38fa6f159da721d5dec1b124c989ec27782cff7.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -12,7 +12,7 @@ return {
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       },
-      appearance = { nerd_font_variant = "normal" },
+      appearance = { nerd_font_variant = "mono" },
       completion = {
         documentation = { auto_show = false }, -- open docs manually; no surprise floats
         menu = { border = "rounded" },
